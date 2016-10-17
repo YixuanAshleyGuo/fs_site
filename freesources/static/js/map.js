aa17a93fe1a8d285907503f06f5bc787fd40901d
YixuanAshleyGuo <yg2430@cumc.columbia.edu>
2016-10-17 16:39:34 -0400

    feedback - confirm before submitting function finished

--- freesources/static/js/map.js
+++ freesources/static/js/map.js
@@ -6,22 +6,11 @@ function onMapClick(e) {
     }
 
 
-function feedback(event){
-//    var targ;
-//    if(!e) var e = window.Event;
-//    if(e.target) targ = e.target;
-//    else if(e.srcElement) targ = e.srcElement;
-//    alert("Test message");
-
-    // not working right now, should be further checked out
-    event.preventDefault();
-    
+function feedback(event,url){    
     var choice  = confirm("Are you sure about submitting feedback?");
-//    var choice = confirm(this.getAttribute('value'));
+
     if(choice){
-        var msg = "Thanks for submitting feedback for this event!";
-        alert(msg);
-        window.location.href = this.getAttribute('href');
+        window.location.href = url;
     }
     
 }
