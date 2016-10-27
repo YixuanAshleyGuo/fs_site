ec1d1834bbe7626f5b0e53b923c2423d3f332824
YixuanAshleyGuo <yg2430@cumc.columbia.edu>
2016-10-27 18:58:49 -0400

    adding marker implemented without inserting into the database

--- freesources/static/js/map.js
+++ freesources/static/js/map.js
@@ -1,8 +1,47 @@
 function onMapClick(e) {
-        L.popup()
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(map);
+        // L.popup()
+            // .setLatLng(e.latlng)
+            // .setContent("You clicked the map at " + e.latlng.toString())
+            // .openOn(map);
+        var newMarker = new L.marker(e.latlng);
+        // var popup="this is a new click event added marker";
+
+                var popup = "<h4>Event Details</h4>";
+                popup += "<table class=\"table\">";
+                popup += "<tr><th>Tag</th><td>";
+                popup += '<input type = "text">';
+                popup += "</td></tr>";
+                popup += "<tr><th>Location</th><td>";
+                popup += '<input type = "text">';
+                popup += "</td></tr>";
+                popup += "<tr><th>Description</th><td>";
+                popup += '<input type = "text">';
+                popup += "</td></tr>";
+                popup += "<tr><th>Type</th><td>";
+                popup += '<input type = "text">';
+                popup += "</td></tr>";
+                popup += "<tr><th>Start Time</th><td>";
+                popup += '<input type = "text">';
+                popup += "</td></tr>";
+                popup += "<tr><th>Expiration</th><td>";
+                popup += '<input type = "text">';
+                popup += "</td></tr>";
+
+
+                popup += '<tr><td>';
+                popup += '<button class="btn btn-success btn-block" onclick="addEvent(this, '+e.latlng.lat+','+e.latlng.lng+')">Submit Event</button></td><td></td></tr>';
+                popup += "</table>"
+
+        newMarker.bindPopup(popup);
+        map.addLayer(newMarker);
+        // $.ajax({
+        //     url:'addEvent',
+        //     type:'POST',
+        //     data:{
+        //         csrfmiddlewaretoken: '{{ csrf_token }}',
+        //         laglng: e.latlng,
+        //     }
+        // })
     }
 
 
