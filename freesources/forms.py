7110b330ba7a62729c3ee9bf943a319e0eba48a7
DyerSituation <ead2174@columbia.edu>
2016-11-29 12:49:44 -0500

    reduced line length in forms.py

--- freesources/forms.py
+++ freesources/forms.py
@@ -18,14 +18,28 @@ TAG_ID = (
 )
 class ItemForm(forms.Form):
     # tag_id = forms.ModelChoiceField(connection.cursor().execute("SELECT tag_id FROM Tag"))
-    tag_name = forms.ChoiceField(choices = TAG_ID, widget=forms.Select(attrs={'class':'form-control'}))
-    expiration_type = forms.ChoiceField(choices = EXP_TYPE, widget=forms.Select(attrs={'class':'form-control exp_type','id':'exp_type'}))
-    latitude = forms.FloatField(required = False, widget=forms.HiddenInput(attrs={'class':'form-control lat' , 'id': 'lat'}))
-    longitude = forms.FloatField(required = False, widget=forms.HiddenInput(attrs={'class':'form-control lng', 'id': 'lng'}))
+    tag_name = forms.ChoiceField(choices = TAG_ID,
+                widget=forms.Select(attrs={'class':'form-control'}))
+    expiration_type = forms.ChoiceField(choices = EXP_TYPE, 
+                widget=forms.Select(attrs={'class':'form-control exp_type','id':'exp_type'}))
+    latitude = forms.FloatField(required = False,
+                widget=forms.HiddenInput(attrs={'class':'form-control lat' , 'id': 'lat'}))
+    longitude = forms.FloatField(required = False,
+                widget=forms.HiddenInput(attrs={'class':'form-control lng', 'id': 'lng'}))
 
     # tag = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),max_length=255)
-    start_time = forms.DateTimeField(required = False, input_formats=['%Y-%m-%dT%H:%M'], widget=forms.DateTimeInput(attrs={'class':'form-control','type':'datetime-local','id':'start_time'}),initial=datetime.now().time())
+    start_time = forms.DateTimeField(required = False,
+                input_formats=['%Y-%m-%dT%H:%M'], 
+                widget=forms.DateTimeInput(attrs={'class':'form-control','type':'datetime-local','id':'start_time'}),
+                initial=datetime.now().time())
     #expiration = forms.DateTimeField(widget=forms.widgets.DateTimeInput(input_formats=["%d %b %Y %H:%M:%S %Z"]))
-    expiration = forms.DateTimeField(required = False, input_formats=['%Y-%m-%dT%H:%M'],widget=forms.DateTimeInput(attrs={'class':'form-control','type':'datetime-local','id':'expiration'}),initial=datetime.now().time())
-    description = forms.CharField(required = False, widget=forms.TextInput(attrs={'class':'form-control'}),max_length=1024)
-    location = forms.CharField(required = False, widget=forms.TextInput(attrs={'class':'form-control location', 'disabled': 'true', 'id': 'location'}),max_length=255)
+    expiration = forms.DateTimeField(required = False,
+                input_formats=['%Y-%m-%dT%H:%M'],
+                widget=forms.DateTimeInput(attrs={'class':'form-control','type':'datetime-local','id':'expiration'}),
+                initial=datetime.now().time())
+    description = forms.CharField(required = False, 
+                widget=forms.TextInput(attrs={'class':'form-control'}),
+                max_length=1024)
+    location = forms.CharField(required = False, 
+                widget=forms.TextInput(attrs={'class':'form-control location', 'disabled': 'true', 'id': 'location'}),
+                max_length=255)
